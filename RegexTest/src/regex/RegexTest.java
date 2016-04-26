1391856fcb37e6e1b5ec1ccc15219c141379bffa
den1den <den_1_den@hotmail.com>
2016-04-26 11:29:35 +0200

    Added tc for FLOAT's

--- RegexTest/src/regex/RegexTest.java
+++ RegexTest/src/regex/RegexTest.java
@@ -56,19 +56,115 @@ public class RegexTest {
         exampleRegexTest.runTest("05", 0);
         exampleRegexTest.runTest("17.245e1", 0);
         
-        testInt(new RegexTest(UNSIGNEDINTEGER));
+        testID(new RegexTest(ID));
+        testNAT(new RegexTest(UNSIGNEDINTEGER));
+        testFLOAT(new RegexTest(REAL));
     }
     
-    public static void testInt(RegexTest tc){
-    	tc.runTestAuto("", 0);
-    	tc.runTestAuto("a", 1);
-    	tc.runTestAuto("aa", 1);
-    	tc.runTestAuto("0", 0);
-    	tc.runTestAuto("a0", 1);
-    	tc.runTestAuto("a0a0a0", 1);
+    public static void testID(RegexTest tc){
+    	System.out.println("testID");
+    	tc.runTestAuto("", false);
+    	tc.runTestAuto("a", true);
+    	tc.runTestAuto("aa", true);
+    	tc.runTestAuto("0", false);
+    	tc.runTestAuto("a0", true);
+    	tc.runTestAuto("a0a0a0", true);
+    	System.out.println();
     }
     
-    public void runTestAuto(String s, int expected){
-    	assert expected == r.run(s, 0);
+    public static void testNAT(RegexTest tc){
+    	System.out.println("testNAT");
+    	tc.runTestAuto("", false);
+    	tc.runTestAuto("a", false);
+    	tc.runTestAuto("0", true);
+    	tc.runTestAuto("01", false);
+    	tc.runTestAuto("001", false);
+    	tc.runTestAuto("1", true);
+    	tc.runTestAuto("173892", true);
+    	tc.runTestAuto("9a9", false);
+    	System.out.println();
+    }
+    
+    public static void testFLOAT(RegexTest tc){
+    	System.out.println("testFLOAT");
+    	System.out.print("-> ");
+    	testNAT(tc);
+    	tc.runTestAuto("0001", false);
+    	System.out.println("-> testFLOAT");
+    	
+    	//Simple positives
+    	tc.runTestAuto("3.1415", true);
+    	tc.runTestAuto("0.0", true);
+    	tc.runTestAuto("0.01", true);
+    	tc.runTestAuto("0.10", true);
+    	tc.runTestAuto(".0", true);
+    	tc.runTestAuto(".01", true);
+    	
+    	//Dot characters
+    	tc.runTestAuto("0e0", true);
+    	tc.runTestAuto("0a0", false);
+    	tc.runTestAuto("0,0", false);
+
+    	//Empty decimals
+    	tc.runTestAuto("0.", false);
+    	tc.runTestAuto("1.", false);
+    	tc.runTestAuto(".", false);
+    	tc.runTestAuto("e.", false);
+    	tc.runTestAuto("a.", false);
+    	
+    	//Negations in e power
+    	tc.runTestAuto("0.e-1", true);
+    	tc.runTestAuto("0.e--1", false);
+    	tc.runTestAuto("0.e---1", false);
+    	tc.runTestAuto("0.e-0", true);		//dd
+    	tc.runTestAuto("0.e0", true);
+    	
+    	//Simple e power positives
+    	tc.runTestAuto("3.14e-7", true);
+    	tc.runTestAuto("0e1", true);
+    	tc.runTestAuto("0e-1", true);
+    	tc.runTestAuto("0.0e1", true);
+    	tc.runTestAuto("0.0e-1", true);
+    	tc.runTestAuto(".0e1", true);
+    	tc.runTestAuto(".0e-1", true);
+    	tc.runTestAuto("0.01e1", true);
+    	tc.runTestAuto("0.01e-1", true);
+    	tc.runTestAuto("0.0001000e100", true);
+    	
+    	//E power characters
+    	tc.runTestAuto("0.0E1", true);
+    	tc.runTestAuto("0.0A1", false);
+    	tc.runTestAuto("0.0exp1", false);
+    	
+    	//E power without decimals
+    	tc.runTestAuto("e", false);
+    	tc.runTestAuto("e1", true);
+    	tc.runTestAuto("e01", false);
+    	tc.runTestAuto("e10", true);
+    	
+    	//More complex E power numbers
+    	tc.runTestAuto("0.e0.e1", false);	// Float in e power
+    	tc.runTestAuto("0.e01", false);		// Invalid int in e power
+    	tc.runTestAuto("0.e1", true);
+    	tc.runTestAuto("0.e0.0", false);
+    	tc.runTestAuto("0.ee", false);
+    	
+    	System.out.println();
+    }
+    
+    public void runTestAuto(String s, boolean match){
+    	int exp = s.length();
+    	int actual = r.run(s, 0);
+    	if(match){
+    		if( actual != exp ) {
+        		System.err.println("Test fail on `" + s + "`, expected match, only got "+actual+"/"+exp);
+        	}
+    	} else {
+    		if( actual == exp ) {
+    			System.err.println("Test fail on `" + s + "`, expected no match, got "+actual+"/"+exp);
+        	}
+    	}
+    	System.out.println("Passed: `" + s + "` (match = "+match+")");
     }
 }
+
