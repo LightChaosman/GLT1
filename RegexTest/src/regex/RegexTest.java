443893e0a0f155365e35723ebd2c2dca37a85c70
LightChaosman <lightchaosman@hotmail.com>
2016-04-26 12:24:36 +0200

    Comments for JAVA

--- RegexTest/src/regex/RegexTest.java
+++ RegexTest/src/regex/RegexTest.java
@@ -10,18 +10,28 @@ import dk.brics.automaton.RunAutomaton;
 public class RegexTest {
 	
 	
+
+	
+	double d = 1;
 	/* Regexes for question 1*/
 	public final static String ID = "[a-z][a-z0-9]*";
 	public final static String UNSIGNEDINTEGER = "[0]|([1-9][0-9]*)";
 	public final static String SIGNEDINTEGER = "-?("+UNSIGNEDINTEGER+")";
 	private final static String TEMP_FLOAT1 = "("+UNSIGNEDINTEGER + ")[eE](" + SIGNEDINTEGER+")";
-	private final static String TEMP_FLOAT2 = "("+UNSIGNEDINTEGER+").([0-9]+)([Ee]"+SIGNEDINTEGER+")?" ;
+	private final static String TEMP_FLOAT2 = "("+UNSIGNEDINTEGER+")?\\.([0-9]+)([Ee]"+SIGNEDINTEGER+")?" ;
 	public final static String REAL = "("+TEMP_FLOAT1+")|("+TEMP_FLOAT2+")|("+UNSIGNEDINTEGER+")";
 	
 	
 	/* Regexes for question 2*/
 	private final static String CHAR = "[a-z]|[A-Z]|\\\"|\\\\";//should backslashes be included?
 	public final static String STRING = "("+CHAR+")*";
+	
+	/* Regexes for question 3*/
+	private final static String SINGLELINECOMMENTCHAR ="[^\\\n]";
+	private final static String JAVASINGLELINE = "//(("+SINGLELINECOMMENTCHAR+")*)";
+	private final static String MULTIINSIDE = "(([^/])*[^/]([^\\*])*)*";
+	private final static String JAVAMULTILINE = "/\\*"+MULTIINSIDE+"\\*/";
+	
 	
 	
     
