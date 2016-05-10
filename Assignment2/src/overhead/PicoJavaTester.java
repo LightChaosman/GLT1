bda2e4044af69636f7ac9d039d88e8591b2946ec
LightChaosman <lightchaosman@hotmail.com>
2016-05-10 19:51:41 +0200

    It now auto executes the .flex & .grammar on windows

--- Assignment2/src/overhead/PicoJavaTester.java
+++ Assignment2/src/overhead/PicoJavaTester.java
@@ -12,8 +12,9 @@ import picoJava.PicoJavaScanner;
 
 public class PicoJavaTester {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		String r = System.getProperty("user.dir");
+		runCommandLine(r);
 		r = r + "\\tests\\PicoJava\\";
 		String rv = r + "valid\\";
 		File f = new File(rv);
@@ -31,6 +32,21 @@ public class PicoJavaTester {
 			runTest(test,false);
 		}
 
+	}
+
+	private static void runCommandLine(String curdir) throws IOException, InterruptedException {
+		if(!System.getProperty("os.name").startsWith("Windows"))return;
+		String beaver = "D:\\eclipse-modeling-mars-2-win32-x86_64\\beaver-0.9.11\\beaver-0.9.11\\lib\\beaver-cc-0.9.11.jar";
+		String flex = "D:\\eclipse-modeling-mars-2-win32-x86_64\\jflex-1.6.1\\jflex-1.6.1\\bin\\jflex.bat"; 
+		Runtime rt = Runtime.getRuntime();
+		flex = flex+" " + curdir + "\\src\\picoJava\\PicoJava.flex";
+		System.out.println(flex);
+		beaver = "java -jar "+beaver+" ";
+		beaver = beaver + curdir +"\\src\\picoJava\\PicoJava.grammar";
+		System.out.println(beaver);
+		rt.exec(beaver);
+		rt.exec(flex);
+		Thread.sleep(500);
 	}
 
 	private static void runTest(File test,boolean valid) throws IOException {
